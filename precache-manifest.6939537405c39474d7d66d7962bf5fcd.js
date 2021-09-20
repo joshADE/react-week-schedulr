cf66d406a490616bf35514daf5789ec08ccccb14
Josh A <joshmail9510@gmail.com>
2021-09-20 17:09:41 -0400

    Updates

---
+++ precache-manifest.6939537405c39474d7d66d7962bf5fcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfbc1904a1d833fa1ced826d70c77645",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9c51aa4e52718908ce2",
+    "url": "./static/css/2.a8f673c4.chunk.css"
+  },
+  {
+    "revision": "2445c3107b3453ea3e6a",
+    "url": "./static/css/main.86a93931.chunk.css"
+  },
+  {
+    "revision": "a9c51aa4e52718908ce2",
+    "url": "./static/js/2.47d4f80f.chunk.js"
+  },
+  {
+    "revision": "c53f65871ab74006a0c4e0b92b224267",
+    "url": "./static/js/2.47d4f80f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2445c3107b3453ea3e6a",
+    "url": "./static/js/main.7eca28ee.chunk.js"
+  },
+  {
+    "revision": "7b7b222461d940ce773d",
+    "url": "./static/js/runtime-main.7335c5c9.js"
+  }
+]);
